f542f53e7978b8121e3c8d4bae24bee34a444a93
ryangholland <rghcode@gmail.com>
2024-07-07 20:56:27 -0400

    Add displayBooks function to dynamically generate book grid

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: "Test Title",
+    author: "Test Author",
+    pages: 100,
+    read: false,
+  },
+  {
+    title: "Test Title 2",
+    author: "Test Author 2",
+    pages: 200,
+    read: false,
+  },
+  {
+    title: "Test Title 3",
+    author: "Test Author 3",
+    pages: 300,
+    read: false,
+  },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,5 +32,53 @@ function addBookToLibrary() {
 }
 
 function displayBooks(myLibrary) {
-    
+  const bookGrid = document.querySelector(".book-grid");
+
+  bookGrid.innerHTML = "";
+  myLibrary.forEach((book) => {
+    displayBook(book);
+  });
 }
+
+function displayBook(book) {
+  const bookGrid = document.querySelector(".book-grid");
+
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  const bookDetails = document.createElement("div");
+  bookDetails.classList.add("book-details");
+  bookDetails.classList.add("flow");
+
+  const bookTitle = document.createElement("h2");
+  bookTitle.textContent = book.title;
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = `by ${book.author}`;
+
+  const bookPages = document.createElement("p");
+  bookPages.textContent = `${book.pages} pages`;
+
+  const bookButtons = document.createElement("div");
+  bookButtons.classList.add("book-btns");
+  bookButtons.classList.add("flow");
+
+  const readBtn = document.createElement("button");
+  readBtn.classList.add("btn");
+  readBtn.classList.add("book-btn");
+  readBtn.textContent = "Not Read";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn");
+  deleteBtn.classList.add("book-btn");
+  deleteBtn.classList.add("del-btn");
+  deleteBtn.textContent = "Delete";
+
+  bookDetails.append(bookTitle, bookAuthor, bookPages);
+  bookButtons.append(readBtn, deleteBtn);
+  bookCard.append(bookDetails, bookButtons);
+
+  bookGrid.appendChild(bookCard);
+}
+
+displayBooks(myLibrary);
